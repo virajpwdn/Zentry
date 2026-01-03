4c025dda4bd54a4093f151acc336baa8bf97998e
viraj <virajpatwardhan6@gmail.com>
2026-01-03 12:25:36 +0530

    feat: vite React folder setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
